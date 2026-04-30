fc356106986bd8b910df1fb3583ea537a1a74fba
ajay-b-2 <ajayb>
2026-04-30 10:44:48 +0530

    updated portfolio design

--- script.js
+++ script.js
@@ -89,3 +89,21 @@ function toggleMenu() {
     const menu = document.getElementById("navLinks");
     menu.classList.toggle("active");
 }
+
+
+
+const elements = document.querySelectorAll('.scroll-left, .scroll-right');
+
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        }
+    });
+}, { threshold: 0.2 });
+
+elements.forEach(el => observer.observe(el));
+
+
+
+
